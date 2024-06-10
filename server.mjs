4dea2a4850a2f66a9effa11a712f5850308dfff2
Pejman H. Nia <81389644+pejmantheory@users.noreply.github.com>
2024-06-09 20:51:18 -0700

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -18,13 +18,18 @@ const pool = new pg.Pool({
 app.use(helmet());
 app.use(cors({
   origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
 }));
+
+app.options('*', cors()); // Enable preflight requests for all routes
+
 app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
